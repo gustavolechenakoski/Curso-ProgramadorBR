2298441e0d13f9768bcaa8ea63e97dd5df48aa4e
Gustavo Lechenakoski <ygustavo07@gmail.com>
2021-04-20 21:07:41 -0300

    Estudo de console e variáveis

--- Javascript - Básico/script.js
+++ Javascript - Básico/script.js
@@ -1,1 +1,31 @@
-alert("Hello Word")
+alert("Hello Word");
+
+console.log("Olá mundo");
+
+var a = "Gustavo Programador";
+console.log(a);
+
+var b;
+
+b = "Gustavo Design";
+
+console.log(b);
+
+var c;
+
+c = "Gustavo Legal";
+
+c = "Gustavo chato";
+
+console.log(c);
+
+
+c = b
+
+console.log(c);
+
+
+var box;
+
+
+
