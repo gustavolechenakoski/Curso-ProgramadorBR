f65f3f4bfdcc345c6558e65fb4631f236ab1fa54
Gustavo Lechenakoski <60752671+gustavolechenakoski@users.noreply.github.com>
2021-06-30 17:50:45 -0300

    react 2

---
+++ React/meu-app/src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+//import './App.css'
+
+
+function Header() {
+
+    return(
+        <Header className="header">
+        <h1>gustavo</h1>
+        <ul className="menu">
+          <li>About</li>
+          <li>Buy</li>
+          <li>contact</li>
+  
+        </ul>
+      </Header>
+    )
+}
+
+export default Header
