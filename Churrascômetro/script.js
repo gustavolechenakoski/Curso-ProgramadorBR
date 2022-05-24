3b5a04b4a0b3ee668b6b0ae12f50eb1683d27381
Gustavo Lechenakoski <60752671+gustavolechenakoski@users.noreply.github.com>
2022-05-24 20:36:35 -0300

    Churrascômetro
    Estudo JS

---
+++ Churrascômetro/script.js
@@ -0,0 +1,57 @@
+// Carne - 400 gr por pessoa  + de 6 horas - 650
+// Cerveja - 1200 ml por Pessoa + 6 horas - 2000 ml
+// Refrigerante/água - 1000 ml por pessoa + 6 horas 1500ml
+// Crianças valem por 0,5
+
+let inputAdultos = document.getElementById("adultos");
+let inputCriancas = document.getElementById("criancas");
+let inputDuracao = document.getElementById("duracao");
+
+
+let resultado = document.getElementById("resultado");
+
+function calcular() {
+    console.log("Calculando...")
+
+    let adultos = inputAdultos.value;
+    let criancas = inputCriancas.value;
+    let duracao = inputDuracao.value;
+
+    let qdtTotalCarne = carnePP(duracao) * adultos + carnePP(duracao) / 2 * criancas;
+    let qdtTotalCerveja = cervejaPP(duracao) * adultos;
+    let qdtTotalBebidas = bebidasPP(duracao) * adultos + bebidasPP(duracao) / 2 * criancas;
+
+    resultado.innerHTML = `<p>${qdtTotalCarne}g de Carna<p>`
+    resultado.innerHTML += `<p>${qdtTotalCerveja}ml de Cerveja<p>`
+    resultado.innerHTML += `<p>${qdtTotalBebidas}ml de Bebidas<p>`
+    
+
+}
+
+function carnePP(duracao) {
+
+    if(duracao >= 6) {
+        return 650;
+    } else {
+        return 400;
+    }  
+}
+
+function cervejaPP(duracao) {
+
+    if(duracao >= 6) {
+        return 2000;
+    } else {
+        return 1200;
+    }  
+}
+
+function bebidasPP(duracao) {
+
+    if(duracao >= 6) {
+        return 1500;
+    } else {
+        return 1000;
+    }  
+}
+
